3fc275b408cf98b01ef73a71755b1dea56f48112
huanito902-cell <huanito902@gmail.com>
2026-09-10 16:59:08 +0500

    Add files via upload

---
+++ lab2/easy.py
@@ -0,0 +1,9 @@
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+c = int(input("Введите третье число: "))
+
+summa = a + b + c
+average = summa / 3
+
+print("Сумма =", summa)
+print("Среднее арифметическое =", average)
